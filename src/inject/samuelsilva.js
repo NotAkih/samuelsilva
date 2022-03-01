b834714a196dade6937ff2b1bdfa8de28c947a00
Jaum <65908658+codeJaum@users.noreply.github.com>
2022-03-01 17:28:15 -0300

    Add files via upload
    Samuel Silva

---
+++ src/inject/samuelsilva.js
@@ -0,0 +1,69 @@
+
+(function ($) {
+
+    var self = {
+        nCageImgs: [
+            'https://images.uncyc.org/pt/4/47/Samuel.JPG'    
+        ],
+        handleImages: function (lstImgs, time) {
+            $.each($('img'), function (i, item) {
+                //Skip if image is already replaced
+                if ($.inArray($(item).attr('src'), lstImgs) == -1) {
+                    var h = $(item).height();
+                    var w = $(item).width();
+
+                    //If image loaded
+                    if (h > 0 && w > 0) {
+                        self.handleImg(item, lstImgs);
+                    }
+                    else {
+                        //Replace when loaded
+                        $(item).load(function () {
+                            //Prevent 'infinite' loop
+                            if ($.inArray($(item).attr('src'), lstImgs) == -1) {
+                                self.handleImg(item, lstImgs);
+                            }
+                        });
+                    }
+                }
+            });
+
+            //Keep replacing
+            if (time > 0) {
+                setTimeout(function () { self.handleImages(lstImgs, time); }, time);
+            }
+        },
+        handleImg: function (item, lstImgs) {
+            $(item).error(function () {
+                //Handle broken imgs
+                self.handleBrokenImg(item, lstImgs);
+            });
+
+            self.setRandomImg(item, lstImgs);
+        },
+        setRandomImg: function (item, lstImgs) {
+            var h = $(item).height();
+            var w = $(item).width();
+            $(item).css('width', w + 'px').css('height', h + 'px');
+            $(item).attr('src', lstImgs[Math.floor(Math.random() * lstImgs.length)]);
+        },
+        handleBrokenImg: function (item, lstImgs) {
+
+            var brokenImg = $(item).attr('src');
+            var index = lstImgs.indexOf(brokenImg);
+            if (index > -1) {
+                lstImgs.splice(index, 1);
+            }
+            self.setRandomImg(item, lstImgs);
+        },
+    };
+
+    //Run on jQuery ready
+    $(function () {
+        self.handleImages(self.nCageImgs, 3000);
+    });
+
+    //Set global variable
+    $.nCage = self;
+
+})(jQuery);
